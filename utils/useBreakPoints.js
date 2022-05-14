4ca07969f1d2fd8b9c358b00fe8dd6ea8e501c1f
AaradDarman <50143241+AaradDarman@users.noreply.github.com>
2022-05-14 08:59:47 +0430

    Add xl Media Query To useBreakPoints hook

--- utils/useBreakPoints.js
+++ utils/useBreakPoints.js
@@ -20,13 +20,15 @@ export default function useBreakpoints() {
   const breakpoints = {
     isXs: useMediaQuery("(max-width: 576px)"),
     isSm: useMediaQuery("(min-width: 577px)"),
-    isMd: useMediaQuery("(min-width: 769px)"),
-    isLg: useMediaQuery("(min-width: 993px)"),
+    isMd: useMediaQuery("(min-width: 768px)"),
+    isLg: useMediaQuery("(min-width: 992px)"),
+    isXl: useMediaQuery("(min-width: 1200px)"),
     active: "xs",
   };
   if (breakpoints.isXs) breakpoints.active = "xs";
   if (breakpoints.isSm) breakpoints.active = "sm";
   if (breakpoints.isMd) breakpoints.active = "md";
   if (breakpoints.isLg) breakpoints.active = "lg";
+  if (breakpoints.isXl) breakpoints.active = "xl";
   return breakpoints;
 }
