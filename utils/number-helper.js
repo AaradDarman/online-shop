6216f3c6f5e71520cd95fc68569000498a82d597
AaradDarman <50143241+AaradDarman@users.noreply.github.com>
2022-05-14 08:58:53 +0430

    Add CompactNumber Function To number-helper.js

--- utils/number-helper.js
+++ utils/number-helper.js
@@ -5,3 +5,17 @@ export const numberWithCommas = (number) => {
 export const numberWithoutCommas = (number) => {
   return number.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
+
+export const compactNumber = (number) => {
+  const suffixes = ["", "هزار", "میلیون", "میلیارد"];
+  const suffixNum = Math.floor(("" + number).length / 4);
+  let shortValue = parseFloat(
+    (suffixNum !== 0 ? number / Math.pow(1000, suffixNum) : number).toPrecision(
+      2
+    )
+  );
+  if (shortValue % 1 !== 0) {
+    shortValue = shortValue.toFixed(1);
+  }
+  return shortValue + " " + suffixes[suffixNum];
+};
