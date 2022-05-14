f3e888cd398248c7ff14f248eb8be27e39600cfc
AaradDarman <50143241+AaradDarman@users.noreply.github.com>
2022-05-14 09:01:08 +0430

    Create withSearch HOC

---
+++ components/HOC/withSearch.jsx
@@ -0,0 +1,52 @@
+import { TextField } from "@mui/material";
+import { rgba } from "polished";
+import { useState } from "react";
+import styled from "styled-components";
+import SearchIcon from "@mui/icons-material/Search";
+
+const StyledInputWraper = styled.div`
+  display: flex;
+  align-items: center;
+  border: 1px solid ${({ theme }) => rgba(theme.palette.text.primary, 0.23)};
+  border-radius: 1rem;
+  color: ${({ theme }) => rgba(theme.palette.text.primary, 0.23)};
+  padding-right: 4px;
+`;
+
+const StyledInput = styled.input`
+  all: unset;
+  padding: 2px 4px;
+  color: ${({ theme }) => theme.palette.text.primary};
+`;
+
+const withSearch = (WrappedComponent) => {
+  const WithSearch = ({ ...props }) => {
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const handleChangeSearch = (e) => {
+      setSearchTerm(e.target.value);
+    };
+
+    return (
+      <>
+        <StyledInputWraper>
+          <SearchIcon fontSize="small" />
+          <StyledInput
+            type="text"
+            onChange={handleChangeSearch}
+            value={searchTerm}
+          />
+        </StyledInputWraper>
+        <WrappedComponent {...props} searchTerm={searchTerm} />
+      </>
+    );
+  };
+  WithSearch.displayName = `WithSearch(${getDisplayName(WrappedComponent)})`;
+  return WithSearch;
+};
+
+export default withSearch;
+
+const getDisplayName = (WrappedComponent) => {
+  return WrappedComponent.displayName || WrappedComponent.name || "Component";
+};
