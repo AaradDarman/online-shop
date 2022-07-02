13ac223f3fbda24a119cee81712bff7ebae33ed9
AaradDarman <50143241+AaradDarman@users.noreply.github.com>
2022-07-02 07:52:57 +0430

    Create Category.jsx

---
+++ components/Header/Category.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+import Link from "next/link";
+import styled from "styled-components";
+
+const StyledItem = styled.li`
+  display: flex;
+  align-items: flex-start;
+  flex-direction: ${({ hasParent }) => hasParent && "column"};
+  list-style: none;
+  background-color: inherit;
+  .subcategories {
+    display: flex;
+    flex-direction: ${({ hasParent }) => hasParent && "column"};
+    background-color: inherit;
+    padding: 0 5px;
+    min-height: 100px;
+    opacity: 0;
+    z-index: -1;
+    transition: 0.3s;
+  }
+  &:hover .subcategories {
+    opacity: 1;
+    z-index: 3 !important;
+  }
+`;
+
+const Category = ({ name, slug, subcategories, parent }) => {
+  const hasChildren = subcategories && subcategories.length;
+  const hasParent = parent;
+
+  return (
+    <StyledItem
+      hasParent={!!hasParent}
+      className={hasParent === null && "parent"}
+    >
+      <Link
+        href={{
+          pathname: "/[category]",
+          query: {
+            title: name,
+          },
+        }}
+        as={`/${slug}`}
+        passHref
+      >
+        <a>
+          <span>{name}</span>
+        </a>
+      </Link>
+      {!!hasChildren && (
+        <div className="subcategories">
+          {subcategories.map((item) => (
+            <Category key={item.name} {...item} />
+          ))}
+        </div>
+      )}
+    </StyledItem>
+  );
+};
+
+export default Category;
