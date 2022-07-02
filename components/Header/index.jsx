8c6021cdbcb1f8e8b8d5c5fb515a3718e67c8723
AaradDarman <50143241+AaradDarman@users.noreply.github.com>
2022-07-02 07:54:19 +0430

    Add CategoryNavigation Section To App

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,15 +1,20 @@
-import React, { memo, useEffect } from "react";
+import React, { memo, useContext, useEffect, useState } from "react";
 
 import styled from "styled-components";
-import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import { Badge, Button, IconButton, Typography } from "@mui/material";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useSelector } from "react-redux";
+import MenuIcon from "@mui/icons-material/Menu";
+import _ from "lodash";
 
 import Icon from "components/shared/Icon";
-import _ from "lodash";
+import CategoryNavigation from "./CategoryNavigation";
+import CategoryNavigationDrawer from "./CategoryNavigationDrawer";
+import useBreakpoints from "utils/useBreakPoints";
+import ThemeToggler from "components/ThemeToggler";
+import { appContext } from "context/app-context";
 
 const StyledHeader = styled.header`
   display: flex;
@@ -37,74 +42,123 @@ const StyledHeader = styled.header`
   a.active {
     color: ${({ theme }) => theme.palette.primary.main};
   }
+  .menu-icon-wraper {
+    margin-top: 58px;
+  }
+`;
+
+const StyledMenuIconWraper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 58px;
 `;
 
 const Header = () => {
   const router = useRouter();
   const { cart, user } = useSelector((state) => state);
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const { isLg } = useBreakpoints();
+  const { toggleTheme } = useContext(appContext);
+
+  const handleOpenDrawer = () => setDrawerOpen(true);
+  const handleCloseDrawer = () => setDrawerOpen(false);
+
+  useEffect(() => {
+    const handleChangeStart = () => setDrawerOpen(false);
+
+    router.events.on("routeChangeStart", handleChangeStart);
+
+    return () => {
+      router.events.off("routeChangeStart", handleChangeStart);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
-    <StyledHeader>
-      <div className="d-flex align-items-center flex-row-reverse">
-        <Badge
-          overlap="circular"
-          anchorOrigin={{
-            vertical: "bottom",
-            horizontal: "left",
-          }}
-          badgeContent={cart?.itemsCount}
-          color="error"
+    <>
+      <StyledHeader>
+        <div className="d-flex align-items-center flex-row-reverse">
+          <Badge
+            overlap="circular"
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+            badgeContent={cart?.itemsCount}
+            color="error"
+          >
+            <Link href="/checkout/cart" passHref>
+              <IconButton
+                color={`${
+                  router.pathname === "/checkout/cart" ? "primary" : "inherit"
+                }`}
+              >
+                <Icon icon="cart" size={24} />
+              </IconButton>
+            </Link>
+          </Badge>
+          <span className="divider d-none d-lg-block" />
+          {_.isEmpty(user.user) ? (
+            <Button variant="outlined" color="inherit">
+              <Link href="/login" passHref>
+                <a className={router.pathname === "/login" ? "active" : ""}>
+                  <Icon icon="login" size={24} className="icon" />
+                  ورود
+                </a>
+              </Link>
+              <span className="divider d-none d-lg-block" />
+              <Link href="/signup">
+                <a
+                  className={`d-none d-lg-block ${
+                    router.pathname === "/signup" ? "active" : ""
+                  }`}
+                >
+                  ثبت نام
+                </a>
+              </Link>
+            </Button>
+          ) : user.user.isAdmin ? (
+            <Link href="/dashboard" passHref>
+              داشبورد
+            </Link>
+          ) : (
+            <Link href="/profile" passHref>
+              <IconButton
+                color={`${
+                  router.pathname === "/profile" ? "primary" : "inherit"
+                }`}
+              >
+                <PersonOutlinedIcon />
+              </IconButton>
+            </Link>
+          )}
+        </div>
+        <Typography
+          variant="h5"
+          component="h1"
+          color={`${router.pathname === "/" ? "primary" : "inherit"}`}
         >
-          <Link href="/checkout/cart" passHref>
-            <IconButton
-              color={`${
-                router.pathname === "/checkout/cart" ? "primary" : "inherit"
-              }`}
-            >
-              <Icon icon="cart" size={24} />
-            </IconButton>
-          </Link>
-        </Badge>
-        <span className="divider" />
-        {_.isEmpty(user.user) ? (
-          <Button variant="outlined" color="inherit">
-            <Link href="/login" passHref>
-              <a className={router.pathname === "/login" ? "active" : ""}>
-                <Icon icon="login" size={24} className="icon" />
-                ورود
-              </a>
-            </Link>
-            <span className="divider" />
-            <Link href="/signup">
-              <a className={router.pathname === "/signup" ? "active" : ""}>
-                ثبت نام
-              </a>
-            </Link>
-          </Button>
-        ) : user.user.isAdmin ? (
-          <Link href="/dashboard" passHref>
-            داشبورد
-          </Link>
-        ) : (
-          <Link href="/profile" passHref>
-            <IconButton
-              color={`${
-                router.pathname === "/profile" ? "primary" : "inherit"
-              }`}
-            >
-              <PersonOutlinedIcon />
-            </IconButton>
-          </Link>
-        )}
-      </div>
-      <Typography
-        variant="h5"
-        component="h1"
-        color={`${router.pathname === "/" ? "primary" : "inherit"}`}
-      >
-        <Link href="/">فروشگاه</Link>
-      </Typography>
-    </StyledHeader>
+          <Link href="/">فروشگاه</Link>
+        </Typography>
+      </StyledHeader>
+
+      {isLg ? (
+        <CategoryNavigation />
+      ) : (
+        <StyledMenuIconWraper>
+          <IconButton onClick={handleOpenDrawer}>
+            <MenuIcon />
+          </IconButton>
+          <ThemeToggler toggleTheme={toggleTheme} />
+        </StyledMenuIconWraper>
+      )}
+      <CategoryNavigationDrawer
+        isOpen={drawerOpen}
+        onClose={handleCloseDrawer}
+        onOpen={handleOpenDrawer}
+      />
+    </>
   );
 };
 
