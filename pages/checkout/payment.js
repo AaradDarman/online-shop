ba123cbfac5d41ca0ba0e0d7a0a850bad746a031
AaradDarman <50143241+AaradDarman@users.noreply.github.com>
2022-07-02 07:57:04 +0430

    Update payment Page

--- pages/checkout/payment.js
+++ pages/checkout/payment.js
@@ -3,20 +3,53 @@ import React from "react";
 import styled from "styled-components";
 import Cookies from "cookies";
 import _ from "lodash";
+import {
+  FormControl,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+  Typography,
+} from "@mui/material";
 
 import MainLayout from "components/layouts/MainLayout";
 import CheckoutLayout from "components/layouts/CheckoutLayout";
 
-const Wraper = styled.div`
-  direction: "rtl";
-  width: 100%;
+const StyledWraper = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
+  direction: "rtl";
+  flex-direction: column;
+  flex: 1;
+  border: 1px solid ${({ theme }) => theme.palette.grey[800]};
+  border-radius: 4px;
+  padding: 1rem;
 `;
 
 const Payment = () => {
-  return <Wraper>Payment</Wraper>;
+  return (
+    <StyledWraper className="ms-0 ms-lg-3">
+      <Typography variant="h6">انتخاب روش پرداخت</Typography>
+      <FormControl>
+        <RadioGroup
+          className="mt-3"
+          aria-labelledby="paymeny-radio-buttons-group"
+          defaultValue="online"
+          name="radio-buttons-group"
+        >
+          <FormControlLabel
+            value="online"
+            control={<Radio />}
+            label="پرداخت اینترنتی"
+          />
+          <FormControlLabel
+            disabled
+            value="pay-in-place"
+            control={<Radio />}
+            label="پرداخت در محل(با کارت بانکی)"
+          />
+        </RadioGroup>
+      </FormControl>
+    </StyledWraper>
+  );
 };
 
 export async function getServerSideProps(ctx) {
